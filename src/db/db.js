656fffe049842eb74678c2f5a21adc95f01cc9a0
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-02-02 16:20:24 +0530

    learnt many to may relationship

--- src/db/db.js
+++ src/db/db.js
@@ -19,9 +19,18 @@ db.sequelize=sequelize;
 db.users=require('../models/userModels')(sequelize,DataTypes);
 db.posts=require('../models/postsModels')(sequelize,DataTypes);
 
+
+//ONE--TO--ONE RELATIONSHIP
 db.users.hasOne(db.posts,{foreignKey:'user_id',as:"postDetails"});
+
+//ONE--TO--MANY RELATIONSHIP
 // db.users.hasMany(db.posts,{foreignKey:'user_id',as:"postDetails"});
+
 db.posts.belongsTo(db.users,{foreignKey:'user_id',as:"userDetails"});
+
+//MANY--TO--MANY RELATIONSHIP  (posts and tags are two different tables) and posts_tags is a differnet table which will have postId and tagId
+// db.posts.belongsToMany(db.tags,{through:db.posts_tags});
+// db.tags.belongsToMany(db.posts,{through:db.posts_tags});
 
 db.sequelize.sync({force:false}).then(()=>{
   console.log("Database has been synced");
